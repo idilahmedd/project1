42197eda3b34bab8634d9fec65c603a68c2950e8
Idilahmed <idil@mac-books-MacBook-Pro.local>
2019-06-12 17:19:16 -0700

    fifth commit

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ var player2Deckurl;
 
 var currentCardsurl;
 var helpCardsurl;
-
+var gameOver;
 
 var score;
 var dealButton;
-var currentCard = [];
-var helpCards = [];
+var currentCards = [];
+var helpDeck = [];
 var player1 = {
     deck: [],
     hand: []
@@ -19,6 +19,21 @@ var player2 = {
     deck: [],
     hand: []
 }
+const values = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "JACK": 11,
+    "ACE": 1,
+    "KING": 13,
+    "QUEEN": 12
+}
 
 //========================================
 //DOM
@@ -26,7 +41,8 @@ var player1Hand = document.getElementsByClassName("hand1")[0];
 var player2Hand = document.getElementsByClassName("hand2")[0];
 var player1Deck = document.getElementById("player1Deck");
 var player2Deck = document.getElementById("player2Deck");
-var currentCards = document.getElementById("curentCards");
+// var currentCards = document.getElementById("curentCards");
+var currentCard = document.getElementById('current')
 var helpCards = document.getElementById("helpCards");
 var player1Score = document.getElementsByClassName("score1")[0];
 var player2Score = document.getElementsByClassName("score2")[0];
@@ -43,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
         deckID = data.deck_id
         player1Deckurl = "https://deckofcardsapi.com/api/deck/"+ deckID + "/draw/?count=20";
         player2Deckurl = "https://deckofcardsapi.com/api/deck/"+ deckID + "/draw/?count=20";
-        hsurl = "https://deckofcardsapi.com/api/deck/"+ deckID + "/draw/?count=12";
+        helpCardsurl = "https://deckofcardsapi.com/api/deck/"+ deckID + "/draw/?count=12";
         console.log(helpCardsurl);
     })
 
@@ -52,8 +68,8 @@ document.addEventListener('DOMContentLoaded', function() {
             return response.json()
         }).then( function(data){
 
-            player1.deck = data.cards.slice(0, 15);
-            player1.hand = data.cards.slice(15);
+            player1.deck = data.cards.splice(0, 15);
+            player1.hand = data.cards.splice(0, 5);
             console.log(player1.hand);
             for (let i = 0; i < player1.hand.length; i++) {
                 // console.log("num children", player1Hand.children.length)
@@ -67,64 +83,74 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch(player2Deckurl).then( function(response) {
             return response.json()
         }).then( function(data){
-            player2.deck = data.cards.slice(0,15);
-            player2.hand = data.cards.slice(15);
-            console.log(player2.deck, player2.hand);
+            player2.deck = data.cards.splice(0, 15);
+            player2.hand = data.cards.splice(0, 5);
+            console.log(player2.deck["1"], player2.hand["0"]);
+            console.log(typeof player2.deck);
         })
     })
     startButton.addEventListener('click', function() {
         fetch(helpCardsurl).then( function(response) {
             return response.json()
         }).then( function(data){
-            helpCards = data.cards;
-            console.log(helpCards);
-            for (let i = 0; i < helpCards.length; i++) {
+            helpDeck = data.cards;
+            console.log(helpDeck);
+            // for (let i = 0; i < helpDeck.length; i++) {
             // console.log("num children", player1Hand.children.length)
-            helpCards.src = helpCards.image;
+            // helpDeck.src = helpDeck.image;
             // console.log("plaeyer1 hand:", player1.hand);
-        }
+        // }
         })
+    })
+
+    helpCards.addEventListener('click', function(e) {
+        // console.log('help!')
+        var card = helpDeck.pop()
+        currentCards.unshift(card);
+        currentCard.src = card.images.png;
+        console.log(currentCards[0]);
+
+    });
+    
+    player1Hand.addEventListener('click', function(e) {
+        console.log('help!')
+        var cardIndex = e.target.id.charAt(e.target.id.length - 1);
+        var clickedCard = player1.hand[cardIndex];
+        console.log("they clicked this card:", clickedCard);
+        console.log("the currentCard is:", currentCards[currentCards.length - 1]);
+        // Check to see if this is a valid choice
+        console.log(values[clickedCard.value], values[(currentCards[0].value)]);
+        if ((values[clickedCard.value] === values[(currentCards[0].value)]) || (values[(currentCards[0]).value ] + 1 === values[clickedCard.value]) || (values[clickedCard.value] - values[(currentCards[0].value)] === -12) ){
+
+        //    currentCard = currentCards[currentCards.length - 1];
+           // adding a card to the hand
+           // make sure to splice card from player hand and push into currentCards array
+           // do the below to switch image on top of currentCards in display
+           currentCard.src = e.target.src;
+           
+           // currentCards.push(value[clickedCard.value]);
+            let newcard  = player1.deck.pop();
+            currentCards.unshift(player1.hand[cardIndex]);
+            player1.hand[cardIndex] = newcard;
+            e.target.src = newcard.images.png;
+            console.log(currentCards[0]);
+        }
+            
+
+            
+         
+        // values[clickedCard.value] === values[currentCards[0].value]
+        // currentCard.src = e.target.src;
+        // e.target.src = player1.deck.pop().images.png;
+        
     })
 })
 
 
 
+    
+
+
+
 
     
-    
-//     //==========================================
-//     //Fetch
-    
-//     fetch(deckurl)
-//     .then(function(responseData){
-//         console.log('get cards');
-//         return responseData.json();
-//     })
-//     .then(function(jsonData){
-//         console.log(jsonData);
-//         deckID = jsonData.deck_id
-//         player1Deckurl = "https://deckofcardsapi.com/api/"+ deckID + "/new/draw/?count=15";
-//         player2Deckurl = "https://deckofcardsapi.com/api/"+ deckID + "/new/draw/?count=15";
-//     })
-//     .then(function() {
-
-//         fetch(player1Deckurl)
-//             .then(function(responseData){
-//                 return responseData.json();
-//             })
-//             .then( function(data) {
-//                 player1Deck = data.json.cards;
-//                 console.log('success')
-//             })
-//             // fetch(player2Deckurl)
-//             //         .then(function(responseData) {
-//             //             return responseData.json();
-//             //         })
-//             //         .then( function(data) {
-//             //             player2Deck = data.json.player2Deckurl;
-//             //             console.log(data)
-//             //         })
-//             })
-// });
-//==========================================
-//Functions
---
+++ js/test.js
@@ -0,0 +1,3 @@
+var a = [0,1,2,3,4,5,6];
+a.splice(2,1);
+console.log(a[1]);
